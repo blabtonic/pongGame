f4acd18f6849ba6cfb74e09ed16cf2a4f4e707b1
Trystan Hendricks <dolkhound@gmx.com>
2014-10-02 10:57:28 -0400

    added p1 and p2 blocks

--- conf.lua
+++ conf.lua
@@ -1,5 +1,6 @@
 function love.conf(t)
-  t.window.title = "Pong" -- Title 
+  t.title = "Pong" -- Title 
   t.window.width = 900 -- Width of window length
   t.window.height = 650 -- Height of window length
+  t.author = "Trystan Hendricks"
 end  
--- main.lua
+++ main.lua
@@ -1,17 +1,22 @@
 function love.load() -- creates the score, loads audio and graphics
   score_p1=0
   score_p2=0
-  position_p1 = 0
+  position_p1 = 0 -- no x axis needed
   position_p2 = 0
+  ball = 0
 end
 
 function love.update(dt) -- will create actions
   if love.keyboard.isDown("w") then
     position_p1 = position_p1 - 1
-    if position_p1 < 0 then position_p1 = 0 end --prevents p1 from going offscreen(up)
+    if position_p1 < 0 then 
+      position_p1 = 0 --prevents p1 from going offscreen(up)
+    end 
   elseif love.keyboard.isDown("s") then
     position_p1 = position_p1+1
-    if position_p1 > love.window.getHeight() then position_p1 = love.window.getHeight() end -- prevents p1 from going offscreen(down)
+    if position_p1 > love.window.getHeight() then -- prevents p1 from going offscreen(down)
+      position_p1 = love.window.getHeight() 
+    end 
   end
   
   if love.keyboard.isDown("up") then
@@ -25,12 +30,16 @@ function love.update(dt) -- will create actions
       position_p2 = love.window.getHeight() 
     end 
   end
-  
 end  
 
 function love.draw() -- this will draw sprites and score
   -- players
-  love.graphics.rectangle("fill",33, 33, 330, 330) -- will resize this 
+  love.graphics.rectangle("fill", 0, position_p1, 15, 60)
+  love.graphics.rectangle("fill", love.window.getWidth()-16, position_p2, 15, 60)
   -- ball
   -- score
 end
+
+function love.keypressed(key)
+  if key == "escape" then love.event.push("quit") end
+end  
